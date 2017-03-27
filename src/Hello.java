e4b81b1f54ef6b038a21e01194fc7173fbf74e75
sevenyuping <926098334@qq.com>
2017-03-27 14:21:33 +0800

    平 修改

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,9 @@
  * Created by ping on 2017/3/25.
  */
 public class Hello {
-  static String  s1= "43";
+    public static void main(String args[]) {
+        final boolean BFLAG = true;
+        if (BFLAG) System.out.println("Always Print Out Me!");
+        else System.out.println("Never Print Out Me!");
+    }
 }
